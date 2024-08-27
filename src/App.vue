b5aacd67796b2235905787eac01b46c9b07d2460
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-08-27 16:38:55 +0200

    [UPD] Milestone 0: screated scaffolding and store.js

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
-<script setup>
+<script>
+import { store } from './store.js';
+import axios from 'axios';
 
+
+export default {
+  components: {
+
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+  data() {
+
+  },
+}
 </script>
 
 <template>
+  <header>
 
+  </header>
+  <main>
+
+  </main>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+@import './styles/generals.scss';
+</style>
